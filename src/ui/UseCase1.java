53b468ea44f8ff5a5be01ed58a130785e30940be
Ziggy Moens <ziggy.moens@student.hogent.be>
2019-02-19 18:22:52 +0100

    Merge origin/master
    Conflicts:
    	src/ui/StartUp.java

--- src/ui/UseCase1.java
+++ src/ui/UseCase1.java
@@ -11,9 +11,41 @@ public class UseCase1 {
     
     public UseCase1(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welkom bij het spel Munchkin - gemaakt door G35\n"
-                + "Wenst u een nieuw spel aan te maken?");
+        
+        Locale nl = new Locale("nl");
+        Locale en = new Locale("en");
+        Locale fr = new Locale("fr");
+        Locale choice;
+        System.out.printf("%s %s%n",ResourceBundle.getBundle("ui/i18n", nl).getString("startUp"),ResourceBundle.getBundle("ui/i18n", nl).getString("languageC"));
+        System.out.printf("%s %s%n",ResourceBundle.getBundle("ui/i18n", en).getString("startUp"),ResourceBundle.getBundle("ui/i18n", en).getString("languageC"));
+        System.out.printf("%s %s%n",ResourceBundle.getBundle("ui/i18n", fr).getString("startUp"),ResourceBundle.getBundle("ui/i18n", fr).getString("languageC"));
+        char gekozenTaal = scan.next().toLowerCase().charAt(0);
+        
+        while(gekozenTaal != 'n' && gekozenTaal != 'f' && gekozenTaal != 'e'){
+            System.out.printf("%s%n",ResourceBundle.getBundle("ui/i18n", nl).getString("wrong"));
+            System.out.printf("%s%n",ResourceBundle.getBundle("ui/i18n", en).getString("wrong"));
+            System.out.printf("%s%n",ResourceBundle.getBundle("ui/i18n", fr).getString("wrong"));
+            gekozenTaal = scan.next().toLowerCase().charAt(0);
+        }
+        
+        switch(gekozenTaal){
+            case 'e': default:
+                choice = new Locale("en");
+                break;
+            case 'f':
+                choice = new Locale("fr");
+                break;
+            case 'n':
+                choice = new Locale("nl");
+                break;
+        }
+        ResourceBundle bundle = ResourceBundle.getBundle("ui/i18n", choice);
+        
+        System.out.printf("%s: %s%n",bundle.getString("picked"), bundle.getString("language"));
+        
+        System.out.println(bundle.getString("newGame"));
         String nieuwSpel = scan.next();
+        
         boolean startUp = false;
         if (nieuwSpel.toLowerCase().charAt(0)=='j' || nieuwSpel.toLowerCase().charAt(0)=='y' || nieuwSpel.toLowerCase().charAt(0)=='o') {
             startUp = true;
@@ -26,7 +58,7 @@ public class UseCase1 {
                 aantalSpelers = scan.nextInt();
             } while (aantalSpelers < 3 || aantalSpelers >7);
             Spel huidigSpel = startSpel(aantalSpelers);
-        }
+        }     
     }
     
     private static Spel startSpel(int aantalSpelers){
