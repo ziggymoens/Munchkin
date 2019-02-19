882c51d3e637c520c6dbeaa71286774199c016eb
Kilian Hoefman <kilian.hoefman@student.hogent.be>
2019-02-19 17:31:01 +0100

    Controles in setters aangebracht volgens domeinregels

--- src/domein/Spel.java
+++ src/domein/Spel.java
@@ -10,7 +10,10 @@ package domein;
  * @author ziggy
  */
 public class Spel {
+
     private int aantalSpelers;
+    private int leeftijd;
+    private String geslacht;
 
     public Spel(int aantalSpelers) {
         setAantalSpelers(aantalSpelers);
@@ -19,10 +22,34 @@ public class Spel {
     public final void setAantalSpelers(int aantalSpelers) {
         this.aantalSpelers = aantalSpelers;
     }
-    
+
     public int getAantalSpelers() {
         return aantalSpelers;
-    }    
+    }
+
+    public final void setLeeftijd(int leeftijd) {
+        if (leeftijd > 0) {
+            this.leeftijd = leeftijd;
+        } else {
+            throw new IllegalArgumentException("Leeftijd moet positief zijn!");
+        }
+    }
+
+    public int getLeeftijd() {
+        return leeftijd;
+    }
+
+    public void setGeslacht(String geslacht) {
+        if (geslacht.toLowerCase().equals("man") || geslacht.toLowerCase().equals("vrouw")) {
+            this.geslacht = geslacht;
+        } else {
+            throw new IllegalArgumentException("Geef een geldig geslacht in.");
+        }
+    }
+
+    public String getGeslacht() {
+        return geslacht;
+    }
 
     public String geefInfo() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
