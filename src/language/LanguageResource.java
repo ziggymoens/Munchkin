4db9a5ade84b9c94a3178f024b77d27ee287b123
Ziggy Moens <ziggy.moens@student.hogent.be>
2019-02-20 10:49:39 +0100

    centraal punt vertalingen aanmaken

---
+++ src/language/LanguageResource.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package language;
+
+import java.util.*;
+
+/**
+ *
+ * @author ziggy
+ */
+public class LanguageResource {
+
+    private Locale locale;
+    private ResourceBundle bundle;
+
+    public LanguageResource() {
+        this(new Locale("en"));
+    }
+    
+    public LanguageResource(Locale locale) {
+        
+        bundle = ResourceBundle.getBundle("ui/i18n", this.locale);
+    }
+
+    public void setLocale(Locale locale) {
+        if (locale.equals(new Locale("en"))||locale.equals(new Locale("nl"))||locale.equals(new Locale("fr"))) {
+            this.locale = locale;
+        }else{
+            this.locale = new Locale("en");
+        }
+        
+    }
+
+    public void setBundle(ResourceBundle bundle) {
+        this.bundle = bundle;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public ResourceBundle getBundle() {
+        return bundle;
+    }
+}
