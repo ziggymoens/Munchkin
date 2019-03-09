ff4f93d8b93ba87ec3388ec868db85838a4bc4d1
Ziggy Moens <ziggy.moens@student.hogent.be>
2019-03-09 15:05:06 +0100

    test

--- src/gui/Test.java
+++ src/gui/Test.java
@@ -6,6 +6,12 @@
 package gui;
 
 import domein.kaarten.Kaart;
+import domein.kaarten.kerkerkaarten.ConsumablesKerker;
+import domein.kaarten.kerkerkaarten.Curse;
+import domein.kaarten.kerkerkaarten.Monster;
+import domein.kaarten.kerkerkaarten.Race;
+import domein.kaarten.schatkaarten.ConsumablesSchat;
+import domein.kaarten.schatkaarten.Equipment;
 import domein.repositories.KaartDbRepository;
 import java.util.List;
 import language.LanguageResource;
@@ -19,6 +25,8 @@ public class Test {
     //i18n testen
     private final LanguageResource bundle = new LanguageResource();
     private final KaartDbRepository kr = new KaartDbRepository();
+    private final List<Kaart> sk = kr.getSchatkaarten();
+    private final List<Kaart> kk = kr.getKerkerkaarten();
 
     public static void main(String[] args) {
         Test test = new Test();
@@ -26,12 +34,36 @@ public class Test {
     }
     
     private String geefKaarten(){
+        int k = 0, s=0;
+        int ck=0,c=0,m=0,r=0,cs=0,e=0;
         String out = "";
-        List<Kaart> kk = kr.getKerkerkaarten();
-        List<Kaart> sk = kr.getSchatkaarten();
         for (Kaart kaart: sk) {
             out += String.format("%s%n", kaart.getNaam());
+            if (kaart instanceof ConsumablesSchat){
+                cs++;
+            }
+            if (kaart instanceof Equipment){
+                e++;
+            }
+            s++;
         }
+        for (Kaart kaart: kk){
+            out += String.format("%s%n", kaart.getNaam());
+            if (kaart instanceof ConsumablesKerker){
+                ck++;
+            }
+            if (kaart instanceof Curse){
+                c++;
+            }
+            if (kaart instanceof Monster){
+                m++;
+            }
+            if (kaart instanceof Race){
+                r++;
+            }
+            k++;
+        }
+        System.err.printf("cs = %d, e = %d, ck = %d, c = %d, m = %d, r = %d, schat = %d, kerker = %d%n", cs, e, ck, c, m, r, s, k);
         return out;
     }
 }
