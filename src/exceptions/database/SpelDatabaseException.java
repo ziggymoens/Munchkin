6b677edb48d9a8fd3a38516e9072983447350aea
Ziggy Moens <ziggy.moens@student.hogent.be>
2019-03-18 20:03:56 +0100

    exception

--- src/exceptions/database/SpelDatabaseException.java
+++ src/exceptions/database/SpelDatabaseException.java
@@ -1,4 +1,23 @@
 package exceptions.database;
 
-public class SpelDatabaseException {
+public class SpelDatabaseException extends RuntimeException{
+    public SpelDatabaseException() {
+        super("SpelDbException, persistentie.mappers.spelMapperDb");
+    }
+
+    public SpelDatabaseException(String message) {
+        super(message);
+    }
+
+    public SpelDatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SpelDatabaseException(Throwable cause) {
+        super(cause);
+    }
+
+    public SpelDatabaseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
