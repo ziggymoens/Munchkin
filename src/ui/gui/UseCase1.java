5c4a597d194a7dbc4504549dfea3fc722ec080b1
Ziggy Moens <ziggy.moens@student.hogent.be>
2019-03-18 20:12:10 +0100

    gui

---
+++ src/ui/gui/UseCase1.java
@@ -0,0 +1,16 @@
+package ui.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UseCase1 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
