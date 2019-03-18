b616e67a7874aba35bd2a5078f79860beba25680
Ziggy Moens <ziggy.moens@student.hogent.be>
2019-03-18 20:41:39 +0100

    rename

--- src/ui/gui/MainGUI.java
+++ src/ui/gui/MainGUI.java
@@ -3,7 +3,7 @@ package ui.gui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class UseCase1 extends Application {
+public class MainGUI extends Application {
 
     public static void main(String[] args) {
         launch(args);
