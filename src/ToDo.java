cf57a776d351d86b39540a5b2b313261dea29f9c
Ziggy Moens <ziggy.moens@student.hogent.be>
2019-03-19 08:27:33 +0100

    to do

--- src/ToDo.java
+++ src/ToDo.java
@@ -1,22 +1,22 @@
 /* TO DO */
 /**
- * Controle van de namen nog aanpassen
+ * Controle van de namen nog aanpassen ==> DONE
  *
  * Java Doc aanvullen in klassen domein.kaarten.
  * 
  * Setters maken in domein.kaarten en toepassen in constructors, illegalargumentexceptions schrijven in meerdere talen.
- * setters maken voor ALLE klassen
+ * setters maken voor ALLE klassen == SEMI DONE
  * 
- * Centraal punt voor exceptions maken in exceptions(Package).
+ * Centraal punt voor exceptions maken in exceptions(Package). ==> DONE
  * 
- * internationalisatie aanvullen, vooral fr, en controleren ==> DONE
+ * internationalisatie aanvullen, vooral fr, en controleren ==> UP TO DATE HOUDEN
  * 
  * 
- * unieke controle toevoegen aan speler ==> DONE, leeftijd verwijderen van lus (want niet gevraagd blijkbaar) ==> DONE, ASCII code checken voor naam
+ * unieke controle toevoegen aan speler ==> DONE, leeftijd verwijderen van lus (want niet gevraagd blijkbaar) ==> DONE, ASCII code checken voor naam ==> DONE
  * 
- * Exception opvangen in applicatie en dan dmv .getMessage() de tekst eruit halen --> String toevoegen aan default constructor van exception.
+ * Exception opvangen in applicatie en dan dmv .getMessage() de tekst eruit halen --> String toevoegen aan default constructor van exception. ==> DONE
  * 
- * SQL exception moet gevangen worden!
+ * SQL exception moet gevangen worden! ==> DONE
  * 
  * COMPERATOR TOEVOEGEN ==> DONE
  * 
@@ -24,13 +24,15 @@
  * 
  * UC3 ==> AFWERKEN
  * 
- * ALLES CATCHEN MAAR ECHT AAAAAALLES
+ * ALLES CATCHEN MAAR ECHT AAAAAALLES ==> UP TO DATE HOUDEN
  * 
- * Niks STATIC tenzij niet anders kan
+ * Niks STATIC tenzij niet anders kan ==> DONE
  * 
  * 
  * 
- * .isBlank() niet gebruiken in setters, veranderen naar .trim().isEmpty -- moeilijk
+ * .isBlank() niet gebruiken in setters, veranderen naar .trim().isEmpty -- moeilijk ==> DONE
+ *
+ * JAVADOCSSSSSSSSS!!!!!!!!!!!!!!
  */
 
 public class ToDo {
