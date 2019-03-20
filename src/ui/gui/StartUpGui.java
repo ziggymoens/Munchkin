22591f95e908b2b578334c8318e9ccf03623afbc
Kilian Hoefman <kilian.hoefman@student.hogent.be>
2019-03-20 22:30:57 +0100

    gui

--- src/ui/gui/StartUpGui.java
+++ src/ui/gui/StartUpGui.java
@@ -1,10 +1,18 @@
 package ui.gui;
 
 import domein.DomeinController;
+import java.util.Locale;
+import java.util.Optional;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import language.LanguageResource;
 import ui.gui.menubar.MenuBarGui;
 import ui.gui.usecase1.UseCase1G;
 
@@ -26,5 +34,23 @@ public class StartUpGui extends Application {
         stage.setTitle("Munchkin - G35");
         stage.show();
         stage.setResizable(false);
+        stage.setOnCloseRequest(
+                new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle(LanguageResource.getString("close"));
+                alert.setHeaderText(LanguageResource.getString("closeconfirm"));
+                alert.setContentText(String.format("%s%n%s", LanguageResource.getString("closetext1"), LanguageResource.getString("closetext2")));
+//                alert.setContentText(LanguageResource.getString("closetext1"));
+//                alert.setContentText(LanguageResource.getString("closetext2"));
+                Optional<ButtonType> antwoord = alert.showAndWait();
+                if (antwoord.get() == ButtonType.CANCEL) {
+                    event.consume();
+                } else {
+                    Platform.exit();
+                }
+            }
+        });
     }
 }
