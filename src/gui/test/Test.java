dfac7976b951a49b2d980e8a7fde7908f931e19b
michi <michi@192.168.0.184>
2019-03-25 21:13:49 +0100

    UC5

---
+++ src/gui/test/Test.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.test;
+
+import domein.DomeinController;
+import domein.kaarten.Kaart;
+import domein.kaarten.kerkerkaarten.ConsumablesKerker;
+import domein.kaarten.kerkerkaarten.Curse;
+import domein.kaarten.kerkerkaarten.Monster;
+import domein.kaarten.kerkerkaarten.Race;
+import domein.kaarten.schatkaarten.ConsumablesSchat;
+import domein.kaarten.schatkaarten.Equipment;
+import domein.repositories.KaartDbRepository;
+import java.util.List;
+
+import language.LanguageResource;
+
+/**
+ * test github intellij kilian
+ * @author ziggy
+ */
+public class Test {
+
+    //i18n testen
+    private final LanguageResource bundle = new LanguageResource();
+    private final KaartDbRepository kr = new KaartDbRepository();
+    private final List<Kaart> sk = kr.getSchatkaarten();
+    private final List<Kaart> kk = kr.getKerkerkaarten();
+    private final DomeinController dc = new DomeinController();
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.geefKaarten());
+    }
+    
+    private String geefKaarten(){
+        int k = 0, s=0;
+        int ck=0,c=0,m=0,r=0,cs=0,e=0;
+        StringBuilder out = new StringBuilder();
+        for (Kaart kaart: sk) {
+            out.append(String.format("%5d:%40s -->%20s%n",kaart.getId(), kaart.getNaam(), kaart.getClass().getSimpleName()));
+            if (kaart instanceof ConsumablesSchat){
+                cs++;
+            }
+            if (kaart instanceof Equipment){
+                e++;
+            }
+            s++;
+        }
+        for (Kaart kaart: kk){
+            out.append(String.format("%5d:%40s -->%20s%n",kaart.getId(), kaart.getNaam(), kaart.getClass().getSimpleName()));
+            if (kaart instanceof ConsumablesKerker){
+                ck++;
+            }
+            if (kaart instanceof Curse){
+                c++;
+            }
+            if (kaart instanceof Monster){
+                m++;
+            }
+            if (kaart instanceof Race){
+                r++;
+            }
+            k++;
+        }
+        System.err.printf("cs = %d, e = %d, ck = %d, c = %d, m = %d, r = %d, schat = %d, kerker = %d%n", cs, e, ck, c, m, r, s, k);
+        return out.toString();
+    }
+}
---
+++ src/gui/ucs/UseCase1.java
@@ -0,0 +1,189 @@
+package gui.ucs;
+
+import domein.*;
+
+import java.util.*;
+
+import exceptions.database.KaartDatabaseException;
+import printer.Printer;
+import exceptions.SpelException;
+import exceptions.SpelerException;
+import printer.ColorsOutput;
+import language.LanguageResource;
+
+/**
+ * @author g35
+ */
+public class UseCase1 {
+
+    //declaraties voor gehele usecase
+    private final Scanner SCAN = new Scanner(System.in);
+    private final DomeinController dc;
+    private final List<Locale> talen;
+    private int aantalSpelers;
+
+    /**
+     * constructor voor UseCase 1
+     *
+     * @param dc Domeincontroller die aangemaakt wordt voor alle use cases in de Main
+     */
+    public UseCase1(DomeinController dc) {
+        this.dc = dc;
+        talen = new ArrayList<>();
+        talen.add(new Locale("nl"));
+        talen.add(new Locale("en"));
+        talen.add(new Locale("fr"));
+        try {
+            welcome();
+        } catch (Exception e) {
+            System.out.print(Printer.exceptionCatch("Exception", e, false));
+        }
+        //gebruiker vragen of hij een nieuw spel wil starten.
+        System.out.println(LanguageResource.getString("newGame"));
+        String nieuwSpel = SCAN.next().toLowerCase();
+        while (!nieuwSpel.equals(LanguageResource.getString("yes")) && !nieuwSpel.equals(LanguageResource.getString("no"))) {
+            System.out.printf(ColorsOutput.decoration("bold") + ColorsOutput.kleur("red") + "%s%n%n", LanguageResource.getString("start.yesno") + ColorsOutput.reset());
+            System.out.println(LanguageResource.getString("newGame"));
+            nieuwSpel = SCAN.next().toLowerCase();
+        }
+        try {
+            if (nieuwSpel.equals(LanguageResource.getString("yes"))) {
+                //th1.start();
+                maakSpel();
+                voegSpelersToe();
+                System.out.println(Printer.printGreen("spel.playersadded"));
+                //verdergaan naar UC2
+                UseCase2 uc2 = new UseCase2(this.dc);
+                uc2.speelSpel(aantalSpelers);
+            } else if (nieuwSpel.equals(LanguageResource.getString("no"))) {
+                System.out.println(Printer.printGreen("gamestop"));
+            }
+        } catch (Exception e) {
+            System.out.print(Printer.exceptionCatch("Exception", e, false));
+        }
+    }
+
+    /**
+     * welcome message in 3 talen
+     */
+    private void welcome() {
+        for (Locale l : talen) {
+            System.out.printf("%s %s%n", LanguageResource.getStringLanguage("startUp", l), LanguageResource.getStringLanguage("languageC", l));
+        }
+        char gekozenTaal = SCAN.next().toLowerCase().charAt(0);
+        //zolang gekozen taal niet voldoet aan beginletter van frans, nederlands of engels
+        while (gekozenTaal != 'n' && gekozenTaal != 'f' && gekozenTaal != 'e') {
+            for (Locale l : talen) {
+                System.out.printf(ColorsOutput.decoration("bold") + ColorsOutput.kleur("red") + "%s%n", LanguageResource.getStringLanguage("wrong", l) + ColorsOutput.reset());
+            }
+            gekozenTaal = SCAN.next().toLowerCase().charAt(0);
+        }
+        //switch voor de verschillende talen
+        Locale choice;
+        switch (gekozenTaal) {
+            case 'e':
+            default:
+                choice = new Locale("en");
+                break;
+            case 'f':
+                choice = new Locale("fr");
+                break;
+            case 'n':
+                choice = new Locale("nl");
+                break;
+        }
+        LanguageResource.setLocale(choice);
+        System.out.printf("%s: %s%n", LanguageResource.getString("picked"), LanguageResource.getString("language"));
+    }
+
+    /**
+     * De gebruiker een aantal spelers laten kiezen, dit tussen 3 en 6 en een
+     * spel aanmaken(grenzen incl.)
+     */
+    private void maakSpel() {
+        int as;
+        boolean tryAgain = true;
+        while (tryAgain) {
+            try {
+                System.out.println(LanguageResource.getString("amountOfPlayers"));
+                as = SCAN.nextInt();
+                System.out.print("\nLoading ");
+                th1.start();
+                dc.startSpel(as);
+                th1.stop();
+                tryAgain = false;
+                this.aantalSpelers = as;
+            } catch (SpelException e) {
+                System.out.print(Printer.exceptionCatch("SpelException", e));
+            } catch (KaartDatabaseException e) {
+                System.out.print(Printer.exceptionCatch("KaartDatabaseException", e));
+            } catch (Exception e) {
+                System.out.println(Printer.exceptionCatch("Exception", e, false));
+                SCAN.nextLine();
+            }
+        }
+        System.out.println("\n" + Printer.printGreen("spel.made"));
+    }
+
+    /**
+     * Voeg het aantal gekozen aantal spelers toe aan het spel a.d.h.v. naam,
+     */
+    private void voegSpelersToe() {
+        for (int i = 0; i < aantalSpelers; i++) {
+            System.out.println(String.format("%s %d", LanguageResource.getString("player"), i + 1));
+            dc.maakSpeler();
+            kiesNaamSpeler(i);
+            kiesGeslachtSpeler(i);
+        }
+    }
+
+    /**
+     * Methode die de gebruiker de naam laat ingeven van de i-de speler
+     *
+     * @param i hoeveelste speler van het spel
+     */
+    private void kiesNaamSpeler(int i) {
+        boolean tryAgain = true;
+        while (tryAgain) {
+            try {
+                System.out.println(LanguageResource.getString("ask.name"));
+                String naam = SCAN.next();
+                dc.geefSpelerNaam(i, naam);
+                tryAgain = false;
+            } catch (SpelerException e) {
+                System.out.print(Printer.exceptionCatch("SpelerException", e));
+            } catch (SpelException e) {
+                System.out.print(Printer.exceptionCatch("SpelException", e));
+            }
+        }
+    }
+
+    /**
+     * Methode die de gebruiker het geslacht laat ingeven van de i-de speler
+     *
+     * @param i hoeveelste speler van het spel
+     */
+    private void kiesGeslachtSpeler(int i) {
+        boolean tryAgain = true;
+        while (tryAgain) {
+            try {
+                System.out.println(LanguageResource.getString("ask.sex"));
+                String geslacht = SCAN.next();
+                dc.geefSpelerGeslacht(i, geslacht);
+                tryAgain = false;
+            } catch (SpelerException e) {
+                System.out.print(Printer.exceptionCatch("SpelerException", e));
+            }
+        }
+    }
+
+    Thread th1 = new Thread(() -> {
+        for (int i = 0; i < 100; i++) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ex) {
+            }
+            System.out.print(".");
+        }
+    });
+}
