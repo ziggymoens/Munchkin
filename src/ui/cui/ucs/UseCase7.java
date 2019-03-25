70753f792ee5522e05d5192f3425864d52935f38
Ziggy Moens <ziggy.moens@student.hogent.be>
2019-03-25 20:39:18 +0100

    uc7 herwerkt wat + opstart

--- src/ui/cui/ucs/UseCase7.java
+++ src/ui/cui/ucs/UseCase7.java
@@ -2,6 +2,7 @@ package ui.cui.ucs;
 
 import domein.DomeinController;
 import language.LanguageResource;
+import printer.ColorsOutput;
 
 import java.util.Scanner;
 
@@ -11,37 +12,51 @@ import java.util.Scanner;
  */
 class UseCase7 {
     private final DomeinController dc;
-    private final Scanner scan = new Scanner(System.in);
+    private final Scanner SCAN = new Scanner(System.in);
 
     UseCase7(DomeinController dc) {
         this.dc = dc;
     }
 
-    public void beheerKaarten(String naam) {
-        maakKeuze();
+    void beheerKaarten(String naam) {
+        maakKeuze(naam);
     }
 
-    public void maakKeuze(){
+    private void maakKeuze(String naam) {
         boolean tryAgain = true;
-        while(tryAgain){
+        while (tryAgain) {
             try {
-                System.out.println(LanguageResource.getString("usecase8.actions"));
-                System.out.println(LanguageResource.getString("usecase8.action1"));
-                System.out.println(LanguageResource.getString("usecase8.action2"));
-                int keuze = scan.nextInt();
-                if(keuze == 1 || keuze == 2) {
-                    tryAgain = false;
+                System.out.println(String.format("%s %s",naam, LanguageResource.getString("usecase8.actions")));
+                System.out.println(String.format("1) %s", LanguageResource.getString("usecase8.action1")));
+                System.out.println(String.format("2) %s", LanguageResource.getString("usecase8.action2")));
+                int keuze = SCAN.nextInt();
+                if (keuze != 1 && keuze != 2) {
+                    throw new Exception();
                 }
-            }catch(Exception e){
-                System.out.println("Foute keuze, probeer opnieuw");
+                tryAgain = false;
+                verwerkKeuze(keuze);
+            } catch (Exception e) {
+                System.out.println(ColorsOutput.kleur("red") + ColorsOutput.decoration("bold") + "Foute keuze, probeer opnieuw" + ColorsOutput.reset());
+                SCAN.nextLine();
+
             }
         }
     }
 
-    public void overzichtAfleggen(){
+    private void verwerkKeuze(int keuze) {
+        switch (keuze) {
+            case 1:
+                break;
+            case 2:
+                break;
+
+        }
     }
 
-    public void overzichtVerkopen(){
+    public void overzichtAfleggen() {
+    }
+
+    public void overzichtVerkopen() {
 
     }
 }
