102fb545cb3d225d58543055f5ab5b2eefc9eef6
Ziggy Moens <ziggy.moens@student.hogent.be>
2019-04-23 09:04:15 +0200

    mapper spel

--- src/printer/Printer.java
+++ src/printer/Printer.java
@@ -2,17 +2,7 @@ package printer;
 
 import language.LanguageResource;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Printer {
-    private static Scanner SCAN = new Scanner(System.in);
-
-    private static Exception ex;
-
     private static Boolean developerMode = false;
 
     /**
@@ -22,20 +12,15 @@ public class Printer {
      * @param e    De gevangen exception
      */
     public static String exceptionCatch(String naam, Exception e) {
-        setException(e);
         String ret = "";
         try {
             ret = String.format(ColorsOutput.decoration("bold") + ColorsOutput.kleur("red") + "%s%s%n%s%n", developerMode ? String.format("%s -> (%s: %s) -> %s: %d, %s: ", e.getStackTrace()[0].getClassName(), LanguageResource.getString("method"), e.getStackTrace()[0].getMethodName(), LanguageResource.getString("line"), e.getStackTrace()[0].getLineNumber(), naam) : "", e.getMessage() == null ? LanguageResource.getString("nomessage") : LanguageResource.getString(e.getMessage()), developerMode && e.getCause() != null ? String.format("%s %s", LanguageResource.getString("cause"), e.getCause()) : "") + ColorsOutput.reset();
-            getInput();
         } catch (Exception ex) {
             ret = String.format("\u001B[31m" + "IllegalArgumentException: %s%n%n", LanguageResource.getString(ex.getMessage()) + "\u001B[0m");
         }
         return ret;
     }
 
-    private static void setException(Exception e) {
-        ex = e;
-    }
 
     /**
      * Print de meegegeven string in de groene kleur naar de terminal
@@ -47,14 +32,12 @@ public class Printer {
     }
 
     public static String exceptionCatch(String naam, Exception e, boolean lang) {
-        setException(e);
         String ret = "";
         if (lang) {
             exceptionCatch(naam, e);
         } else {
             try {
                 ret = String.format(ColorsOutput.decoration("bold") + ColorsOutput.kleur("red") + "%s%s%n%n", developerMode ? String.format("%s -> (%s: %s) -> %s: %d, %s: ", e.getStackTrace()[0].getClassName(), LanguageResource.getString("method"), e.getStackTrace()[0].getMethodName(), LanguageResource.getString("line"), e.getStackTrace()[0].getLineNumber(), naam) : "", e.getMessage() == null ? LanguageResource.getString("nomessage") : e.getMessage(), developerMode && e.getCause() != null ? String.format("%s %s", LanguageResource.getString("cause"), e.getCause()) : "") + ColorsOutput.reset();
-                getInput();
             } catch (Exception ex) {
                 ret = String.format("\u001B[31m" + "IllegalArgumentException: %s%n%n", ex.getMessage() + "\u001B[0m");
             }
@@ -64,55 +47,5 @@ public class Printer {
 
     public static void setDeveloperMode(Boolean developerMode) {
         Printer.developerMode = developerMode;
-    }
-
-
-    private static String str = "";
-    private Runnable task;
-    private TimerTask timerTask;
-
-//    private void schedule (Runnable runnable, long delay){
-//        task = runnable;
-//        timerTask = new TimerTask() {
-//            @Override
-//            public void run() {
-//                task.run();
-//            }
-//        };
-//        this.schedule(timerTask, delay);
-//    }
-//
-//    private void reschedule(long delay){
-//        timerTask.cancel();
-//        timerTask = new TimerTask() {
-//            @Override
-//            public void run() {
-//                task.run();
-//            }
-//        };
-//        schedule(timerTask, delay);
-//    }
-
-    static task = new TimerTask() {
-        public void run() {
-            if (str.equals("[a-zA-Z0-9].*")) {
-                //System.out.println( "you input nothing. exit..." );
-                ex.printStackTrace();
-                //System.exit( 0 );
-            }
-        }
-    };
-
-    public static void getInput() throws Exception {
-
-        Timer timer = new Timer();
-        timer.schedule(task, 10 * 100);
-
-        //System.out.println( "Input a string within 10 seconds: " );
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(System.in));
-        str = in.readLine();
-        timer.cancel();
-        //System.out.println( "you have entered: "+ str );
     }
 }
