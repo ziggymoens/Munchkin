808a0cd010eca4000f9d3e53afeb04921fd4ad4e
JonathanVandenEynden <jonathan.vandeneyndenvanlysebeth@student.hogent.be>
2019-04-26 00:50:39 +0200

    Aanpassing algemeen en vordering van UC5

--- src/ui/cui/ucs/UseCase5.java
+++ src/ui/cui/ucs/UseCase5.java
@@ -1,48 +1,63 @@
 package ui.cui.ucs;
 
 import domein.DomeinController;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 import java.util.Scanner;
 
+import domein.Speler;
 import domein.kaarten.Kaart;
 import domein.kaarten.kerkerkaarten.ConsumablesKerker;
+import domein.kaarten.kerkerkaarten.Curse;
+import domein.kaarten.kerkerkaarten.Monster;
+import domein.kaarten.kerkerkaarten.Race;
 import domein.kaarten.schatkaarten.ConsumablesSchat;
+import domein.kaarten.schatkaarten.Equipment;
 import language.LanguageResource;
+import java.util.List;
 
 public class UseCase5 {
 
     private final DomeinController dc;
-    private int speler;
+    private int spelerAanBeurt;
     private final Scanner SCAN = new Scanner(System.in);
-    private boolean runAway;
 
-    public UseCase5(DomeinController dc, String naam, boolean runAway) {
+
+    public UseCase5(DomeinController dc, int i) {
         this.dc = dc;
-        this.speler = dc.geefSpelerAanBeurt();
-        this.runAway = runAway;
+        this.spelerAanBeurt = i;
     }
 
     void speelKaart() {
-        System.out.println(dc.toonOverzichtKaartenInHand(speler));
+        System.out.println(dc.toonOverzichtKaartenInHand(spelerAanBeurt));
         System.out.println(LanguageResource.getString("usecase5.choicecard"));
-        int kaart = SCAN.nextInt();
-        System.out.println(dc.geefSpeler(speler).getKaarten().get(kaart - 1));
-        //validatieKaart(kaart);
+        int kaart = 0;
+        try{
+            kaart = SCAN.nextInt();
+        }catch(Exception e){
+            System.err.println("Je moet de juiste input geven");
+        }
+        System.out.println(dc.geefSpeler(spelerAanBeurt).getKaarten().get(kaart - 1));
+        if(validatieKaart(kaart)){
+
+        }else{
+            System.out.println("Kaart mag niet gespeeld worden");
+        }
     }
 
-    /*private Boolean validatieKaart(int kaart){
-        Kaart kr = dc.geefSpeler(speler).getKaarten().get(kaart - 1);
-        if(runAway){
-            if(kr instanceof ConsumablesSchat){
-                    ((ConsumablesSchat) kr).
-
-            }else{
-                return false;
-            }
+    private Boolean validatieKaart(int kaart){
+        Kaart kr = dc.geefSpeler(spelerAanBeurt).getKaarten().get(kaart - 1);
+        //Kaarten die de Tegenspelers mogen spelen
+        if(dc.geefTegenspelers().contains(dc.geefNaamSpeler(dc.geefSpelerAanBeurt()))){
+            if(kr instanceof ConsumablesSchat || kr instanceof ConsumablesKerker || kr instanceof Equipment || kr instanceof Race || kr instanceof Monster){
+                return true;
+            }return false;
+            //Kaarten die de Speler mag spelen
         }else{
-
+            if(kr instanceof ConsumablesSchat || kr instanceof ConsumablesKerker || kr instanceof Monster || kr instanceof Curse){
+                //Aanpassen dat als speler geen hulp wou, alleen negatieve ConsumablesKerker gespeeld mag worden
+                return true;
+            }
         }
-    }*/
+        return false;
+    }
 }
