a136b2d066dd67462628581874fedd26cca3c1c7
JonathanVandenEynden <jonathan.vandeneyndenvanlysebeth@student.hogent.be>
2019-05-10 01:36:12 +0200

    KaartAfhandelen

--- src/ui/gui/kaart_afhandeler/KaartAfhandeler.java
+++ src/ui/gui/kaart_afhandeler/KaartAfhandeler.java
@@ -1,17 +1,23 @@
 package ui.gui.kaart_afhandeler;
 
 import domein.DomeinController;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-
+import javafx.stage.Stage;
+import language.LanguageResource;
 import java.util.HashMap;
 import java.util.Map;
 
-public class KaartAfhandeler {
+public class KaartAfhandeler extends BorderPane{
     private DomeinController dc;
     private BorderPane center;
     private int kaart;
     private Map<String, Runnable> kaartAfhandelen;
+    private boolean help;
+    private int spelerAanBeurt = dc.geefSpelerAanBeurt();
 
     public KaartAfhandeler(DomeinController dc, BorderPane center) {
         this.dc = dc;
@@ -33,7 +39,22 @@ public class KaartAfhandeler {
     }
 
     private void monster() {
-
+        //this.getChildren().clear();
+        Stage stage = new Stage();
+        BorderPane borderPane = new BorderPane();
+        Scene scene = new Scene(borderPane, 500,500);
+        stage.setTitle("Monster");
+        Label vraag = new Label(LanguageResource.getString("usecase4.ask.help"));
+        Button btnJa = new Button(LanguageResource.getString("yes"));
+        Button btnNee = new Button(LanguageResource.getString("no"));
+        btnJa.setOnAction(this::wiltHulp);
+        btnNee.setOnAction(this::wiltGeenHulp);
+        borderPane.setTop(vraag);
+        borderPane.setLeft(btnNee);
+        borderPane.setRight(btnJa);
+        stage.setScene(scene);
+        stage.show();
+        //toonKaartenVanSpeler(borderPane);
     }
 
     private void geenEffectKaart() {
@@ -44,5 +65,19 @@ public class KaartAfhandeler {
         dc.curseKaart(dc.geefNaamSpeler(dc.geefSpelerAanBeurt()));
     }
 
+    private void wiltHulp(ActionEvent event){
+        help = true;
+    }
+
+    private void wiltGeenHulp(ActionEvent event){
+        help = false;
+    }
+
+   private void toonKaartenVanSpeler(BorderPane borderPane ){
+        //borderPane.getChildren().clear();
+        dc.geefIDKaartenInHand(dc.geefNaamSpeler(spelerAanBeurt));
+        //ImageView ivKaart = new ImageView(new )
+
+    }
 
 }
