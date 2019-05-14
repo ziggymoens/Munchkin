0df4892d124f8aff9656d41f21680eab80853470
michiel <michiel.murphy@student.hogent.be>
2019-05-14 10:34:00 +0200

    GUI 8 EN 9

--- src/ui/gui/ucs/usecase8/UseCase8G.java
+++ src/ui/gui/ucs/usecase8/UseCase8G.java
@@ -1,8 +1,6 @@
 package ui.gui.ucs.usecase8;
 
 import domein.DomeinController;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -12,8 +10,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import ui.gui.a_universal.maingui.MainGui;
-
-import java.util.List;
 
 public class UseCase8G extends MainGui {
 
@@ -31,13 +27,7 @@ public class UseCase8G extends MainGui {
     private void buildGui() {
         VBox box;
         box = new VBox();
-        cboTitels = new ComboBox();
-        cboTitels.setPromptText("Kies hier het spel dat u wilt opslaan");
-        //mogelijkheden plaatsen voor spellen
-        List<String> spelen = dc.geefOverzichtSpelen();
-        ObservableList lijst = FXCollections.observableArrayList(spelen);  //FXCollections omdat je niet zomaar new ObservableList() kunt doen
-        cboTitels.setItems(lijst);
-        lblNaam = new Label("Geef het gekozen spel een naam: ");
+        lblNaam = new Label("Geef het spel een naam: ");
         lblBevestiging = new Label();
         txfNaam = new TextField();
         txfNaam.setEditable(true);
@@ -46,9 +36,10 @@ public class UseCase8G extends MainGui {
             @Override
             public void handle(ActionEvent event) {
                 lblBevestiging.setText("Het spel is succesvol opgeslaan.");
+                dc.spelOpslaan();
             }
         });
-        box.getChildren().addAll(cboTitels, lblNaam, txfNaam, bevestig, lblBevestiging);
+        box.getChildren().addAll(lblNaam, txfNaam, bevestig, lblBevestiging);
         this.getPane().setCenter(box);
         this.setPadding(new Insets(25));   //ruimte aan zijkanten
         //this.setSpacing(10); //tussenruimte tussen 2 componenten
--- src/ui/gui/ucs/usecase9/UseCase9G.java
+++ src/ui/gui/ucs/usecase9/UseCase9G.java
@@ -1,9 +1,35 @@
 package ui.gui.ucs.usecase9;
 
+import domein.DomeinController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
 import ui.gui.a_universal.maingui.MainGui;
 
+import java.util.List;
+
+
 public class UseCase9G extends MainGui{
-    public UseCase9G() {
+    private ComboBox cboSpelen;
+    private DomeinController dc;
+
+    public UseCase9G(DomeinController dc) {
         getStylesheets().add("ui/gui/ucs/usecase9/UseCase9G.css");
+        this.dc = dc;
+        buildGui();
+    }
+
+
+    private void buildGui() {
+        VBox box;
+        box = new VBox();
+        cboSpelen = new ComboBox();
+        cboSpelen.setPromptText("Kies hier het spel dat u wilt opslaan");
+        //mogelijkheden plaatsen voor spellen
+        List<String> spelen = dc.geefOverzichtSpelen();
+        ObservableList lijst = FXCollections.observableArrayList(spelen);  //FXCollections omdat je niet zomaar new ObservableList() kunt doen
+        cboSpelen.setItems(lijst);
+        box.getChildren().addAll(cboSpelen);
     }
 }
